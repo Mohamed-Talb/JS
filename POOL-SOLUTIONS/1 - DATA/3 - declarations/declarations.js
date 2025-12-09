1db6b0327acc9351dffa3c55910b37523641d8c1
soul <mo7amed.talb@gmail.com>
2025-12-09 21:33:07 +0100

    exercices: 1-DATA/how-2-js and 1-DATA/2-primitives

---
+++ POOL-SOLUTIONS/1 - DATA/3 - declarations/declarations.js
@@ -0,0 +1,52 @@
+console.log({'awsome key': 'great value'});
+console.log({'second key': 25});
+
+console.log({
+    'first key':1,
+    'second key': 22,
+    'third key': 333,
+    'fourth key': 4444,
+    'the last key': 5555,
+});
+
+const myStreet = {
+    number: 175,
+    label: 'boulvard',
+    name: 'matabiau',
+}
+
+const address = {
+  country: 'France',
+  town: 'Toulouse',
+  postalCode: 31000,
+  street: myStreet,
+}
+
+// console.log(myStreet);
+
+// const address = {
+//     country: 'France',
+//     town: 'paris',
+//     postalCode: 31000,
+//     street: myStreet,
+// }
+
+// console.log({
+//     firstname: 'clement',
+//     lastname: 'denis',
+//     email: 'mo7amed.talb@gmail.com',
+//     age: 24,
+//     address : address,
+// })
+
+
+const user =  
+{
+    firstname: 'clement',
+    lastname: 'denis',
+    email: 'mo7amed.talb@gmail.com',
+    age: 24,
+    address,
+}
+
+console.log(user);
